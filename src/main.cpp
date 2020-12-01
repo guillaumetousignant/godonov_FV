05f73b8ef06c17e7304462ec9a4db7c7db442449
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-01 16:41:25 -0500

    n_points

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,44 @@
 
 int main(void) {
     constexpr int n_problems = 4;
-    constexpr int n_points = 1000;
+    constexpr int n_resolutions = 4;
+    constexpr int n_points_analytic = 1000;
 
-    double rho[4][2] = {{2.281, 1.408},
-                        {1.045, 3.483},
-                        {1.598, 2.787},
-                        {4.696, 1.408}};
+    const int n_points[] = {100,
+                            200,
+                            500,
+                            1000};
 
-    double u[4][2] = {{164.83, 0.0},
-                      {200, 200},
-                      {-383.64, -216.97},
-                      {0.0, 0.0}};
+    const double rho[4][2] = {{2.281, 1.408},
+                              {1.045, 3.483},
+                              {1.598, 2.787},
+                              {4.696, 1.408}};
 
-    double p[4][2] = {{201.170e3, 101.1e3},
-                      {300.0e3, 300.0e3},
-                      {91.88e3, 200.0e3},
-                      {404.4e3, 101.1e3}};
+    const double u[4][2] = {{164.83, 0.0},
+                            {200, 200},
+                            {-383.64, -216.97},
+                            {0.0, 0.0}};
+
+    const double p[4][2] = {{201.170e3, 101.1e3},
+                            {300.0e3, 300.0e3},
+                            {91.88e3, 200.0e3},
+                            {404.4e3, 101.1e3}};
     
-    double time[] = {12.0e-3,
-                     25.0e-3,
-                     35.0e-3,
-                     7.0e-3};
+    const double time[] = {12.0e-3,
+                           25.0e-3,
+                           35.0e-3,
+                           7.0e-3};
 
-    double discontinuity[] = {2.0,
-                              2.0,
-                              5.0,
-                              5.0};
+    const double discontinuity[] = {2.0,
+                                    2.0,
+                                    5.0,
+                                    5.0};
     
     std::vector<Problem_t> problems;
     problems.reserve(4);
 
     for (int i = 0; i < n_problems; ++i) {
-        problems.push_back(Problem_t(rho[i][0], rho[i][1], u[i][0], u[i][1], p[i][0], p[i][1], time[i], discontinuity[i], n_points, i + 1));
+        problems.push_back(Problem_t(rho[i][0], rho[i][1], u[i][0], u[i][1], p[i][0], p[i][1], time[i], discontinuity[i], n_points_analytic, i + 1));
     }
 
     // Starting actual computation
