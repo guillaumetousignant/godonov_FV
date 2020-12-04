c296b49984f3df7602345b7d8039afd01b0df19e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-04 15:12:42 -0500

    plotting everything stack overflows

--- plots.py
+++ plots.py
@@ -173,8 +173,9 @@ def generate_figures(save_path, result_exact, result, title, suffix):
 N = [100, 200, 500, 1000]
 
 result_exact = find_problem_files("exact")
-result_riemann = find_problem_files_N("riemann", N)
-result_roe = find_problem_files_N("roe", N)
+#result_riemann = find_problem_files_N("riemann", N)
+#result_roe = find_problem_files_N("roe", N)
+#result_roe_entropy = find_problem_files_N("roe_entropy", N)
 result_hlle = find_problem_files_N("hlle", N)
 
 
@@ -182,8 +183,10 @@ result_hlle = find_problem_files_N("hlle", N)
 save_path = Path.cwd() / "figures"
 save_path.mkdir(parents=True, exist_ok=True)
 
-generate_figures(save_path, result_exact, result_riemann, "Riemann problem", "riemann")
-generate_figures(save_path, result_exact, result_roe, "Roe", "roe")
+# Plotting everything at the same time will stack overflow.
+#generate_figures(save_path, result_exact, result_riemann, "Riemann problem", "riemann")
+#generate_figures(save_path, result_exact, result_roe, "Roe", "roe")
+#generate_figures(save_path, result_exact, result_roe_entropy, "Roe entropy fix", "roe_entropy")
 generate_figures(save_path, result_exact, result_hlle, "HLLE", "hlle")
 
 plt.show()
