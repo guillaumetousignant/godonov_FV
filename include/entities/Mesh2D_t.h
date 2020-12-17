bcdb79e24e32d8b7503c290aa2841c763f9f7535
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-17 14:20:46 -0500

    removed useless stuff

--- include/entities/Mesh2D_t.h
+++ include/entities/Mesh2D_t.h
@@ -21,17 +21,6 @@ namespace FVM { namespace Entities {
             std::vector<std::vector<size_t>> node_to_cell_;
             std::vector<Face_t> faces_;
 
-            std::vector<double> a_;
-            std::vector<double> u_;
-            std::vector<double> p_;
-            std::vector<double> x_;
-            std::vector<double> gamma_;
-            double delta_x_;
-            int n_faces_;
-            std::vector<double> F_1_; // ρu
-            std::vector<double> F_2_; // ρu² + p
-            std::vector<double> F_3_; // u(γp/(γ-1) + ρu²/2)
-
             void initial_conditions(double a_L, double a_R, double u_L, double u_R, double p_L, double p_R, double x_L, double x_R, double gamma_L, double gamma_R, double discontinuity);
 
         private:
--- src/entities/Mesh2D_t.cpp
+++ src/entities/Mesh2D_t.cpp
@@ -22,21 +22,7 @@ FVM::Entities::Mesh2D_t::Mesh2D_t(std::filesystem::path filename) {
 FVM::Entities::Mesh2D_t::~Mesh2D_t() {}
 
 void FVM::Entities::Mesh2D_t::initial_conditions(double a_L, double a_R, double u_L, double u_R, double p_L, double p_R, double x_L, double x_R, double gamma_L, double gamma_R, double discontinuity) {
-    for (int i = 0; i < n_cells_ + 2; ++i) { // No need for explicit treatment of the boundary conditions, nice
-        x_[i] = x_L + (i - 0.5) * delta_x_;
-        if (x_[i] < discontinuity) {
-            a_[i] = a_L;
-            u_[i] = u_L;
-            p_[i] = p_L;
-            gamma_[i] = gamma_L;
-        }
-        else {
-            a_[i] = a_R;
-            u_[i] = u_R;
-            p_[i] = p_R;
-            gamma_[i] = gamma_R;
-        }
-    }
+    
 }
 
 void FVM::Entities::Mesh2D_t::readSU2(std::filesystem::path filename){
