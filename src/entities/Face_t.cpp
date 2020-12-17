d243946399f7018147426da25f781bbbe99b2162
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-17 13:52:17 -0500

    forgot namespace

--- src/entities/Face_t.cpp
+++ src/entities/Face_t.cpp
@@ -2,7 +2,7 @@
 
 FVM::Entities::Face_t::Face_t() {}
 
-Face_t(size_t node_0, size_t node_1, size_t cell_0, size_t cell_1) : 
+FVM::Entities::Face_t::Face_t(size_t node_0, size_t node_1, size_t cell_0, size_t cell_1) : 
     nodes_{node_0, node_1},
     cells_{cell_0, cell_1} {}
 
