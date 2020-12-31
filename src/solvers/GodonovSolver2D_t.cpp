f48f452adb58a02e4953b3d367c3cef9f6768145
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-31 11:27:34 -0500

    forgot ()

--- src/solvers/GodonovSolver2D_t.cpp
+++ src/solvers/GodonovSolver2D_t.cpp
@@ -26,7 +26,7 @@ template class GodonovSolver2D_t<RoeEntropyFlux_t, VenkatakrishnanLimiter_t>;
 template class GodonovSolver2D_t<HLLEFlux_t, VenkatakrishnanLimiter_t>;
 
 template<typename FluxCalculator, typename FluxLimiter>
-GodonovSolver2D_t<FluxCalculator, FluxLimiter>::GodonovSolver2D_t() : {}
+GodonovSolver2D_t<FluxCalculator, FluxLimiter>::GodonovSolver2D_t() {}
 
 template<typename FluxCalculator, typename FluxLimiter>
 void GodonovSolver2D_t<FluxCalculator, FluxLimiter>::solve(double end_time, double cfl, FVM::Entities::Mesh2D_t& mesh) {
